44734b7ce2a54070ffd0be8cee380d412aedc51e
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2025-03-20 15:11:40 +0900

    first commit

---
+++ src/main/java/com/example/teachingSharePoint/TeachingSharePointApplication.java
@@ -0,0 +1,13 @@
+package com.example.teachingSharePoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachingSharePointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachingSharePointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teachingSharePoint/TeachingSharePointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teachingSharePoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachingSharePointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
